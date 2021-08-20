239528414da6be8bf968c630d75fd400c12ecf29
Hajime Nakagami <nakagami@gmail.com>
2021-08-20 15:30:30 +0900

    suppress warnings.

--- src/statement.rs
+++ src/statement.rs
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-use super::wireprotocol::*;
+#![allow(dead_code)]
 use super::xsqlvar::*;
 use super::Connection;
 use super::Error;
