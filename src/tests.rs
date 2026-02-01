5512320aaaa15b92ed705360c55190bc7dd7ed23
Hajime Nakagami <nakagami@gmail.com>
2026-02-01 15:58:11 +0900

    modify test

--- src/tests.rs
+++ src/tests.rs
@@ -96,8 +96,8 @@ fn test_connnect() {
     .unwrap();
 
     conn.execute(
-        "insert into foo(a, b, c, e, g, i, j) values (?, 'A', 'B', '1999-01-25', '00:00:01', 0.1, 0.1)",
-        params!(2)
+        "insert into foo(a, b, c, e, g, i, j) values (?, ?, ?, '1999-01-25', '00:00:01', 0.1, 0.1)",
+        params![2, "A", "B"]
     )
     .unwrap();
     conn.execute("insert into foo(a, b, c, e, g, i, j) values (3, 'X', 'Y', '2001-07-05', '00:01:02', 0.2, 0.2)", ()).unwrap();
